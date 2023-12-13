1f37546bd6688d22b782795fe0bf7e3389ab4e2f
tgvdufuture <contact.clowntrone.minipato@gmail.com>
2023-12-13 15:43:29 +0100

    index

--- search.php
+++ search.php
@@ -1,3 +1,11 @@
+<?php
+session_start(); // Déplacez cette ligne vers le début du fichier
+include('connexion_bd.php');
+
+// Récupérer le pseudo de l'utilisateur actuel depuis la session
+$pseudo = isset($_SESSION['pseudo']) ? $_SESSION['pseudo'] : null;
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,7 +21,7 @@
     <nav class="navbar">
         <div class="search-container">
             <div class="buttons-nav">
-                <a href="./index.php"><button class="button-nav">Accueil</button></a>
+                <a href="./accueil.php"><button class="button-nav">Accueil</button></a>
                 <a href=""><button class="button-nav">Voir plus</button></a>
             </div>
             <div class="search-wrapper">
@@ -30,37 +38,31 @@
             </div>
         </div>
 
-        <div class="avatar">
-            <?php
-            session_start();
-            include('connexion_bd.php');
+     <div class="avatar">
+        <?php
 
-            // Récupérer le pseudo de l'utilisateur actuel depuis la session
-            $pseudo = $_SESSION['pseudo'];
-
-            // Requête SQL avec une jointure pour récupérer l'ID de l'avatar de l'utilisateur
-            $requete_avatar_user = "SELECT avatars.id, avatars.nom_avatar FROM utilisateurs
+        // Requête SQL avec une jointure pour récupérer l'ID de l'avatar de l'utilisateur
+        $requete_avatar_user = "SELECT avatars.id, avatars.nom_avatar FROM utilisateurs
                         JOIN avatars ON utilisateurs.avatar_id = avatars.id
                         WHERE utilisateurs.pseudo = ?";
 
-            $requete_preparee_avatar_user = $connexion->prepare($requete_avatar_user);
-            $requete_preparee_avatar_user->bind_param("s", $pseudo);
-            $requete_preparee_avatar_user->execute();
-            $resultat_avatar_user = $requete_preparee_avatar_user->get_result();
+        $requete_preparee_avatar_user = $connexion->prepare($requete_avatar_user);
+        $requete_preparee_avatar_user->bind_param("s", $pseudo);
+        $requete_preparee_avatar_user->execute();
+        $resultat_avatar_user = $requete_preparee_avatar_user->get_result();
 
-            if ($resultat_avatar_user && $resultat_avatar_user->num_rows > 0) {
-                $row_avatar_user = $resultat_avatar_user->fetch_assoc();
-                $id_avatar = $row_avatar_user['id'];
-                $nom_avatar_base64 = $row_avatar_user['nom_avatar'];
+        if ($resultat_avatar_user && $resultat_avatar_user->num_rows > 0) {
+            $row_avatar_user = $resultat_avatar_user->fetch_assoc();
+            $id_avatar = $row_avatar_user['id'];
+            $nom_avatar_base64 = $row_avatar_user['nom_avatar'];
 
-                echo "<a href='profile.php'><img src='$nom_avatar_base64' alt='Avatar' class='avatar'></a>";
-            } else {
-                // Gérer le cas où l'avatar n'est pas trouvé
-                echo "Avatar non trouvé";
-            }
-            ?>
-
-        </div>
+            echo "<a href='profile.php'><img src='$nom_avatar_base64' alt='Avatar' class='avatar'></a>";
+        } else {
+            // Gérer le cas où l'avatar n'est pas trouvé
+            echo "Avatar non trouvé";
+        }
+        ?>
+    </div>
     </nav>
     <?php
     include('connexion_bd.php');
@@ -103,5 +105,14 @@
 
 </body>
 <script src="./index.js"></script>
+ <script>
+        // Sélectionner le lien qui a un attribut title contenant "000webhost.com"
+        var lien = document.querySelector('a[title*="000webhost.com"]');
 
+        // Vérifier si le lien existe et que l'attribut alt de l'image à l'intérieur est égal à "www.000webhost.com"
+        if (lien && lien.querySelector('img').alt === "www.000webhost.com") {
+            // Appliquer la propriété display: none; si l'attribut alt est présent
+            lien.style.display = 'none';
+        }
+</script>
 </html>
